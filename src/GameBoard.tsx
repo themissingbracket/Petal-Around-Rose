8af2863ea97a212edaff91092fc6e0e15b5c4da3
Rahul <themissingbracket@gmail.com>
2020-10-15 13:38:59 +0800

    shit not working

--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -3,7 +3,20 @@ import Dice from './Dice'
 import GameEngine from './GameEngine'
 import './gameboard.css'
 
+type Notification = {
+  status: 'success' | 'error' | 'none'
+  message?: string
+}
+
+function buildNotification(status?: 'success' | 'error', message?: string): Notification {
+  return {
+    status: status || 'none',
+    message: message
+  }
+}
+
 const GameBoard: React.FC = () => {
+    const [ notification, setNotification ] = useState<Notification>(buildNotification('success', 'hello world'))
     const [dice, setDice ] = useState<number[]>(GameEngine.roll())
     const resetGame = () => {
         setDice(GameEngine.roll())
@@ -11,14 +24,19 @@ const GameBoard: React.FC = () => {
 
     return (
         <div className='game__board'>
+            <div className="notification__container">
+              <div className={`notification ${notification.status}`}>
+                {notification.message}
+              </div>
+            </div>
             <div className="dice__container">
                 {
                     dice.map((roll, idx) => <Dice key={`dice__${idx}`} count={roll}/>)
                 }
             </div>
             <div className="input__container">
-                <label htmlFor="petals"> Number of Petals around the Rose ???</label>
-                <input name='petals' type="text" placeholder='Number of petals around rose??' />
+                <label className='petal__label' htmlFor="petals"> Number of Petals around the Rose ???</label>
+                <input name='petals' className='petal__input' type="number" placeholder='Number of petals around rose??' />
             </div>
             <div className="control__container">
                 <div className="button submit__button">Submit</div>
