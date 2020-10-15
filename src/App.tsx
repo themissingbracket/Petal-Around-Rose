56c8355d64c7ce89cf0d98b6b98adaaccc0b776b
Rahul <k.dasgupta112@gmail.com>
2020-10-15 20:01:08 +0800

    Added label

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import GameBoard from './GameBoard';
 function App() {
   return (
     <div className="App">
+      <div className="title__container">
+        <h2>Petals around Rose</h2>
+      </div>
       <GameBoard />
     </div>
   );
