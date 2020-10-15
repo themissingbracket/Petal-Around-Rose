6804425366ca4db45be79ffb51d6eac14b694ffe
Rahul <k.dasgupta112@gmail.com>
2020-10-15 19:58:48 +0800

    Dev Complete

--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Dice from './Dice'
-import GameEngine from './GameEngine'
+import GameEngine, { GameState } from './GameEngine'
 import './gameboard.css'
+import gameEngine from './GameEngine'
 
 type Notification = {
   status: 'success' | 'error' | 'none'
   message?: string
 }
 
-function buildNotification(status?: 'success' | 'error', message?: string): Notification {
+function buildNotification(status?: 'success' | 'error' | 'none', message?: string): Notification {
   return {
     status: status || 'none',
     message: message
@@ -16,30 +17,55 @@ function buildNotification(status?: 'success' | 'error', message?: string): Noti
 }
 
 const GameBoard: React.FC = () => {
-    const [ notification, setNotification ] = useState<Notification>(buildNotification('success', 'hello world'))
-    const [dice, setDice ] = useState<number[]>(GameEngine.roll())
+    const [ notification, setNotification ] = useState<Notification>(buildNotification())
+    const [ gameState, setGameState ] = useState<GameState>(gameEngine.buildGame())
+    const inputRef = useRef<HTMLInputElement>(null)
     const resetGame = () => {
-        setDice(GameEngine.roll())
+        setGameState(GameEngine.buildGame())
     }
-
+    const checkGame = () => {
+      if(inputRef.current) {
+        const { value } = inputRef.current
+        const newState = GameEngine.checkGame(parseInt(value), gameState)
+        if (newState.correctAnswers === 4 && newState.status === 'success') {
+          const notification =  buildNotification('success', "Congratulations! You have worked out the secret! \n Make sure you don't tell anyone!")
+          setNotification(notification)
+        } else if (newState.correctAnswers < 4 && newState.incorrectAnswers % 4 === 0 && newState.status ==='error') {
+          const notification =  buildNotification('error', 'Hint: The name of the game is important... Petals Around the Rose.')
+          setNotification(notification)
+        } else {
+          const notification =  buildNotification(newState.status, newState.message)
+          setNotification(notification)
+        }
+        setTimeout(() => {
+          setNotification(buildNotification())
+        }, 3000)
+        setGameState(newState)
+        inputRef.current.value = ''
+      }
+    }
     return (
         <div className='game__board'>
             <div className="notification__container">
               <div className={`notification ${notification.status}`}>
-                {notification.message}
+                {
+                  notification.message?.split('\n').map((value, idx) =>
+                  <span key={`message_${idx}`}>{value}</span>
+                  )
+                }
               </div>
             </div>
             <div className="dice__container">
                 {
-                    dice.map((roll, idx) => <Dice key={`dice__${idx}`} count={roll}/>)
+                    gameState.dice.map((roll, idx) => <Dice key={`dice__${idx}`} count={roll}/>)
                 }
             </div>
             <div className="input__container">
                 <label className='petal__label' htmlFor="petals"> Number of Petals around the Rose ???</label>
-                <input name='petals' className='petal__input' type="number" placeholder='Number of petals around rose??' />
+                <input ref={inputRef} name='petals' className='petal__input' type="number" placeholder='Number of petals around rose??' />
             </div>
             <div className="control__container">
-                <div className="button submit__button">Submit</div>
+                <div className="button submit__button" onClick={checkGame}>Submit</div>
                 <div className="button reset__button" onClick={resetGame}>
                     Reset
                 </div>
--- src/GameEngine.ts
+++ src/GameEngine.ts
@@ -1,18 +1,60 @@
 
+export type GameState = {
+  dice: number[]
+  status: 'success' | 'error' | 'none'
+  message: string
+  correctAnswers: number
+  incorrectAnswers: number
+
+}
+
 export interface GameEngine {
-    roll(diceCounts?: number): number[]
-    
+    buildGame( previousState?: GameState ):GameState
+    checkGame(guessedValue:number, state: GameState): GameState
+}
+
+const DICE_TYPE = 6
+
+function calculatePetals(dice: number[]): number {
+  return dice.reduce((acc, value) => {
+    if(value === 3 ) acc = acc + 2
+    else if(value === 5) acc = acc + 4
+    return acc
+  }, 0)
 }
 
 const gameEngine: GameEngine = {
-    roll:(diceCounts: number = 6) => {
-        const rolls: number[] = []
-        for(let i=0; i<diceCounts; i++) {
-            const randInt = Math.floor(Math.random() *6 ) + 1
-            rolls.push(randInt)
-        }
-        return rolls
+    buildGame(previousState?:GameState): GameState{
+      const dice:number[] = []
+      for(let i=0;i<6; i++) {
+        dice.push(Math.floor(Math.random() * 6)+1)
+      }
+      const correctAnswers = previousState?.correctAnswers || 0
+      const incorrectAnswers = previousState?.incorrectAnswers || 0
+      return {
+        dice,
+        correctAnswers,
+        incorrectAnswers,
+        status: 'none',
+        message: ''
+      }
+    },
+    checkGame(guessedValue: number, state : GameState): GameState{
+      const petals = calculatePetals(state.dice)
+      const updatedState = this.buildGame(state)
+      if( guessedValue % 2 === 0) {
+        const isGuessCorrect = guessedValue === petals
+        updatedState.status =  isGuessCorrect ? 'success' : 'error'
+        updatedState[isGuessCorrect ? 'correctAnswers' : 'incorrectAnswers'] = updatedState[isGuessCorrect ? 'correctAnswers' : 'incorrectAnswers'] + 1
+        updatedState.message =  isGuessCorrect ? 'Well done! You guessed it!' : `No sorry, it;s ${petals} not ${guessedValue}.`
+      } else {
+        updatedState.status = 'error'
+        updatedState.message = `No sorry, it's ${petals} not ${guessedValue}. The score is always even.`
+        updatedState.incorrectAnswers = updatedState.incorrectAnswers + 1
+      }
+      return updatedState
     }
+
 }
 
 export default gameEngine
